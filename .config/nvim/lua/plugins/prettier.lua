1d0428d4b51bf43c0ea93a8d2f7f81e460ac1b68
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2024-05-17 14:10:13 -0300

    Testing the upgrade

--- .config/nvim/lua/plugins/prettier.lua
+++ .config/nvim/lua/plugins/prettier.lua
@@ -1,20 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "prettierd")
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.prettierd)
-    end,
-  },
-  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
